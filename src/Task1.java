809735e86d47bbc6a69c1dc2e604d9f6eb38863b
NisaNajafli <ncfliniss@gmail.com>
2026-06-18 14:30:13 +0400

    task completed

---
+++ src/Task1.java
@@ -0,0 +1,19 @@
+public class BankAccountInfo{
+
+    public static void main(String[] args) {
+
+        String customerName = "Nisa Najafli" ;
+        int accountNumber = 23;
+        double balance = 424.24;
+        boolean isAccountActive = true;
+        String accountType = "Emanet";
+
+
+        System.out.println("Müştəri:" + " " +  customerName);
+        System.out.println("Hesab nömrəsi:" + " " +  accountNumber);
+        System.out.println("Balans:" + " " +  balance);
+        System.out.println("Hesab aktivdir mi:" + " " +  isAccountActive);
+        System.out.println("Hesab növü:" + " " +  accountType);
+    }
+
+}
